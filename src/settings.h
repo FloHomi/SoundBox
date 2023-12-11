4056916d5c5acd3f16f6739296d60cca8d2ae601
FloHomi <Florian.Homilius@Dataschalt.com>
2023-12-11 09:02:22 +0100

    Add support for BQ2589Xship mode in system.cpp

--- src/settings.h
+++ src/settings.h
@@ -43,10 +43,10 @@
 	#define HEADPHONE_ADJUST_ENABLE         // Used to adjust (lower) volume for optional headphone-pcb (refer maxVolumeSpeaker / maxVolumeHeadphone) and to enable stereo (if PLAY_MONO_SPEAKER is set)
 	//#define PLAY_MONO_SPEAKER             // If only one speaker is used enabling mono should make sense. Please note: headphones is always stereo (if HEADPHONE_ADJUST_ENABLE is active)
 	#define SHUTDOWN_IF_SD_BOOT_FAILS       // Will put ESP to deepsleep if boot fails due to SD. Really recommend this if there's in battery-mode no other way to restart ESP! Interval adjustable via deepsleepTimeAfterBootFails.
-	#define MEASURE_BATTERY_VOLTAGE         // Enables battery-measurement via GPIO (ADC) and voltage-divider
+	// #define MEASURE_BATTERY_VOLTAGE         // Enables battery-measurement via GPIO (ADC) and voltage-divider
 	//#define MEASURE_BATTERY_MAX17055      // Enables battery-measurement via external fuel gauge (MAX17055)
-	//#define MEASURE_BATTERY_BQ2589X		  	// Enables battery-measurement via external Battery Manager (BQ2589X)
-	//#define USE_SHIP_MODE_BQ2589X			// Enables ship mode for BQ2589X. Puts the whole system into a super power mode. (12µA consumption) Can only be woken up by pressing the QON button.
+	#define MEASURE_BATTERY_BQ2589X		  	// Enables battery-measurement via external Battery Manager (BQ2589X)
+	#define USE_SHIP_MODE_BQ2589X			// Enables ship mode for BQ2589X. Puts the whole system into a super power mode. (12µA consumption) Can only be woken up by pressing the QON button.
 	#define SHUTDOWN_ON_BAT_CRITICAL      // Whether to turn off on critical battery-level (only used if MEASURE_BATTERY_XXX is active)	//#define PLAY_LAST_RFID_AFTER_REBOOT   // When restarting ESPuino, the last RFID that was active before, is recalled and played
 	//#define USE_LAST_VOLUME_AFTER_REBOOT  // Remembers the volume used at last shutdown after reboot
 	#define USEROTARY_ENABLE                // If rotary-encoder is used (don't forget to review WAKEUP_BUTTON if you disable this feature!)
